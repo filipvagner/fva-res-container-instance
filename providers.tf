4910ecad934537b53a2a450162871dcc8b42a896
Filip Vagner <filip.vagner@orbit.cz>
2026-02-15 20:35:26 +0100

    Removed unused providers

--- providers.tf
+++ providers.tf
@@ -4,13 +4,5 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "< 5.0.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5.0, < 5.0.0"
-    }
-    azapi = {
-      source = "Azure/azapi"
-      version = "~> 2.0"
-    }
   }
 }
